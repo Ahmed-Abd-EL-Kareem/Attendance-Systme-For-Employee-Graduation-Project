8bf3365b290c2efc98178107d501815e37c706f9
Ahmed-Abd-EL-Kareem <137029582+Ahmed-Abd-EL-Kareem@users.noreply.github.com>
2025-07-11 16:20:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       <Route path="/profile" element={<Profile />} />
       <Route path="/history" element={<History />} /> */}
           <Route path="/employee/:id/*" element={<EmployeeRoute />} />
-          <Route path="/*" element={<NotFound />} />
+          // <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
